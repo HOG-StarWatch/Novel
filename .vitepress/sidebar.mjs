1eacf0a8b2eff7cc221414a895c97817d48c4593
HOG-StarCraft <W3389823633@hotmail.com>
2025-04-20 15:30:22 +0800

    add file

--- .vitepress/sidebar.mjs
+++ .vitepress/sidebar.mjs
@@ -2,6 +2,7 @@ import generateSidebar from "../utils/auto-gen-sidebar.mjs";
 
 export default {
     '/Novel/': generateSidebar("Novel"),
+    '/N/': generateSidebar("N"),
     '/Pixiv/': generateSidebar("Pixiv"),
     '/Comix/': generateSidebar("Comix"),
 };
